25f5c6a7e85a2fe20d22c2d363272147ea60fed6
Evan Wang <evan7@students.cs.ubc.ca>
2021-10-04 15:29:38 -0700

    Reminder to add sub-bounding boxes for enemy sprites (for more exact arrow impact)

--- src/physics_system.cpp
+++ src/physics_system.cpp
@@ -6,18 +6,21 @@
 vec2 get_bounding_box(const Motion& motion)
 {
 	// abs is to avoid negative scale due to the facing direction.
+	
 	return { abs(motion.scale.x), abs(motion.scale.y) };
 }
 
 // This is a SUPER APPROXIMATE check that puts a circle around the bounding boxes and sees
 // if the center point of either object is inside the other's bounding-box-circle. You can
 // surely implement a more accurate detection
+// TODO: Add exact bounding box struct for each enemy
+// And check if custom bounding boxes 
 bool collides(const Motion& motion1, const Motion& motion2)
 {
 	vec2 dp = motion1.position - motion2.position;
 	float dist_squared = dot(dp,dp);
 	const vec2 other_bonding_box = get_bounding_box(motion1) / 2.f;
-	const float other_r_squared = dot(other_bonding_box, other_bonding_box);
+	const float other_r_squared =  dot(other_bonding_box, other_bonding_box);
 	const vec2 my_bonding_box = get_bounding_box(motion2) / 2.f;
 	const float my_r_squared = dot(my_bonding_box, my_bonding_box);
 	const float r_squared = max(other_r_squared, my_r_squared);
