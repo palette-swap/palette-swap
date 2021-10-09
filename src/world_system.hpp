9497fb24ff7ab28255a895815464a81d24ecac60
Dylan <dylanmb@students.cs.ubc.ca>
2021-10-08 22:58:34 -0700

    M1 Final Submission
    - fix name mismatches
    - fix meshPtrs naming against convention

--- src/world_system.hpp
+++ src/world_system.hpp
@@ -43,7 +43,7 @@ public:
 private:
 	// Input callback functions
 	void on_key(int key, int /*scancode*/, int action, int mod);
-	void on_mouse_move(vec2 pos);
+	void on_mouse_move(vec2 mouse_position);
 	void on_mouse_click(int button, int action, int /*mods*/);
 	void on_mouse_scroll(float offset);
 
