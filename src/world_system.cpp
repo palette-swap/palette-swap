ef4c136bb444b49ef94dc3a46291c42cf0f60a5b
Evan Wang <evan7@students.cs.ubc.ca>
2021-10-19 21:53:35 -0700

    Renamed Wendigo asset to Treeant

--- src/world_system.cpp
+++ src/world_system.cpp
@@ -246,12 +246,12 @@ void WorldSystem::restart_game()
 	// Creates all enemy instance
 	// TODO: Load them throughout the level dynamically/from preset file
 	// TODO: Change input from texture selection to generalized enemy type selection/index
-	Entity armor1 = create_enemy(uvec2(64, 56), TEXTURE_ASSET_ID::WENDIGO);
+	Entity armor1 = create_enemy(uvec2(64, 56), TEXTURE_ASSET_ID::TREEANT);
 	registry.colors.get(armor1) = { 2, 1, 1 };
-	Entity wen1 = create_enemy(uvec2(65, 56), TEXTURE_ASSET_ID::WENDIGO);
-	registry.colors.get(wen1) = { 1, 1, 1 };
-	Entity wen2 = create_enemy(uvec2(66, 56), TEXTURE_ASSET_ID::WENDIGO);
-	registry.colors.get(wen2) = { 1, 1, 1 };
+	Entity tree1 = create_enemy(uvec2(65, 56), TEXTURE_ASSET_ID::TREEANT);
+	registry.colors.get(tree1) = { 1, 1, 1 };
+	Entity tree2 = create_enemy(uvec2(66, 56), TEXTURE_ASSET_ID::TREEANT);
+	registry.colors.get(tree2) = { 1, 1, 1 };
 	Entity armor2 = create_enemy(uvec2(64, 53), TEXTURE_ASSET_ID::ARMOR);
 	registry.colors.get(armor2) = { 1, 1, 2 };
 	Entity raven = create_enemy(uvec2(40, 54), TEXTURE_ASSET_ID::RAVEN);
