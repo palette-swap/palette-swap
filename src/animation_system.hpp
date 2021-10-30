67255841263a42cfc1b60974580ab6b5da2b9e5a
Evan Wang <evan7@students.cs.ubc.ca>
2021-10-29 19:29:07 -0700

    Tweeked running speed to be a bit faster

--- src/animation_system.hpp
+++ src/animation_system.hpp
@@ -26,7 +26,7 @@ static constexpr float player_animation_speed = 1.2;
 // Used for animation event speeds for the player
 static constexpr float player_melee_speed = 2.5;
 static constexpr float player_heavy_melee_speed = 1;
-static constexpr float player_running_speed = 1.8;
+static constexpr float player_running_speed = 2;
 static constexpr float player_blue_red_switch_speed = 1;
 // Value denoting the animation states for the player
 // KEEP ALIGNED WITH STATES REPRESENTED IN PLAYER SPRITESHEET
