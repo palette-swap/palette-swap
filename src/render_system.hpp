2a4d8093981e0f18a9057fec66ff91e6ed3eda0d
Dylan <dylanmb@students.cs.ubc.ca>
2021-10-30 13:45:54 -0700

    Fix texture leftover from old text impl

--- src/render_system.hpp
+++ src/render_system.hpp
@@ -35,7 +35,7 @@ class RenderSystem {
 	// for each corresponding enemy/player type, such that it grabs general folder path
 	// and then uses path to grab sprites, stats, and behaviour
 	// Make sure these paths remain in sync with the associated enumerators.
-	const std::array<std::string, texture_count-1> texture_paths = {
+	const std::array<std::string, texture_count> texture_paths = {
 			textures_path("./01/Player Spritesheet.png"),
 			textures_path("./Slime/Slime Spritesheet.png"),
 			textures_path("./Living Armor/Living Armor Spritesheet.png"),
