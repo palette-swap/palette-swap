f6d84f85499037b9b1df94c7de0948d3c5661c46
Dylan <dylanmb@students.cs.ubc.ca>
2021-10-31 16:12:31 -0700

    Remove max size

--- src/world_system.cpp
+++ src/world_system.cpp
@@ -93,7 +93,7 @@ GLFWwindow* WorldSystem::create_window(int width, int height)
 	glfwSetCursorPosCallback(window, cursor_pos_redirect);
 	glfwSetMouseButtonCallback(window, mouse_click_redirect);
 	glfwSetScrollCallback(window, scroll_redirect);
-	glfwSetWindowSizeLimits(window, GLFW_DONT_CARE, GLFW_DONT_CARE, 1920, 1080);
+	glfwSetWindowSizeLimits(window, GLFW_DONT_CARE, GLFW_DONT_CARE, GLFW_DONT_CARE, GLFW_DONT_CARE);
 	glfwSetFramebufferSizeCallback(window, resize_redirect);
 
 	//////////////////////////////////////
