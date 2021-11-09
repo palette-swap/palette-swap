1343b967a5d41f20cc42ddfc1586a2f2c4f895e7
Dylan <dylanmb@students.cs.ubc.ca>
2021-11-09 03:34:45 -0800

    Show/hide inventory with i and Esc

--- src/ui_system.cpp
+++ src/ui_system.cpp
@@ -2,7 +2,12 @@
 
 void UISystem::on_key(int key, int action, int mod)
 {
-	if (action == GLFW_RELEASE && key == GLFW_KEY_ESCAPE) { }
+	if (action == GLFW_PRESS && key == GLFW_KEY_I) {
+		registry.get<UIGroup>(inventory_group).visible = true;
+	}
+	if (action == GLFW_PRESS && key == GLFW_KEY_ESCAPE) {
+		registry.get<UIGroup>(inventory_group).visible = false;
+	}
 }
 
 void UISystem::on_left_click(dvec2 mouse_screen_pos) { }
