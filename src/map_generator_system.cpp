b6ae9f0bb432cd10cb2853188e4ef81a716476cf
Evan Wang <evan7@students.cs.ubc.ca>
2021-11-21 23:03:28 -0800

    Tuned speeds a bit more

--- src/map_generator_system.cpp
+++ src/map_generator_system.cpp
@@ -185,7 +185,7 @@ static void load_enemy(unsigned int enemy_index, const rapidjson::Document& json
 	// Need to replace with a different component denoting a boss enemy
 	if (enemy_component.type == EnemyType::KingMush) {
 		enemy_animation.max_frames = 8;
-		enemy_animation.speed_adjustment = 0.6;
+		enemy_animation.speed_adjustment = 0.6f;
 		visible = false;
 	} else {
 		enemy_animation.max_frames = 4;
