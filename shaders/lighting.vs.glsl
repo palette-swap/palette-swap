72be50ace10ba4d7a4c98f44339a8650c20e10d4
Dylan <dylanmb@students.cs.ubc.ca>
2021-12-02 13:50:56 -0800

    Explicitly set lighting in_position

--- shaders/lighting.vs.glsl
+++ shaders/lighting.vs.glsl
@@ -1,13 +1,13 @@
 #version 330
 
 // Input attributes
-in vec3 in_position;
+layout (location = 0) in vec2 in_position;
 
 // Passed to fragment shader
 out vec2 texcoord;
 
 void main()
 {
-    gl_Position = vec4(in_position.xy, 0, 1.0);
+    gl_Position = vec4(in_position, 0, 1.0);
 	texcoord = (in_position.xy + 1) / 2.f;
 }
