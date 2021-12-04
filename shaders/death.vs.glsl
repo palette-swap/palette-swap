a4959bc358ba63c76847b7ff1de25e6b2fc2bc4d
Evan Wang <evan7@students.cs.ubc.ca>
2021-12-03 16:56:44 -0800

    Changed death shader slightly

--- shaders/death.vs.glsl
+++ shaders/death.vs.glsl
@@ -24,6 +24,9 @@ uniform float num_states = 8;
 uniform int frame;
 uniform int state;
 
+// Value for maximum folding length
+uniform float max_fold_length = 0.25f;
+
 void main()
 {
 	texcoord = in_texcoord;
@@ -44,26 +47,26 @@ void main()
 	
 
 //	 Distorts the death animation sinusoidally
-	distort_position.x += 0.02 * sin(time);
-	distort_position.y += 0.02 * sin(time);
+	distort_position.x += 0.01 * sin(time/2);
+	distort_position.y -= 0.01 * sin(time);
 
 	if (frame >= 1) {
 		if ((texcoord.y - top_side)/height <= 0.4) {
-			distort_position.y += 0.25 * abs((texcoord.y - (top_side + 0.25)))/0.25;
+			distort_position.y += max_fold_length * abs((texcoord.y - (top_side + 0.25)))/0.25;
 		}
 
 		if ((texcoord.y - top_side)/height >= 0.6) {
-			distort_position.y -= 0.25 * abs((texcoord.y - (bottom_side - 0.25)))/0.25;
+			distort_position.y -= max_fold_length * abs((texcoord.y - (bottom_side - 0.25)))/0.25;
 		}
 	}
 
 	if (frame >= 3) {
 		if ((texcoord.x - left_side)/width <= 0.4) {
-			distort_position.x += 0.25 * abs((texcoord.x - (left_side + 0.25)))/0.25;
+			distort_position.x += max_fold_length * abs((texcoord.x - (left_side + 0.25)))/0.25;
 		}
 
 		if ((texcoord.x - left_side)/width >= 0.6) {
-			distort_position.x -= 0.25 * abs((texcoord.x - (right_side - 0.25)))/0.25;
+			distort_position.x -= max_fold_length * abs((texcoord.x - (right_side - 0.25)))/0.25;
 		}
 	}
 
